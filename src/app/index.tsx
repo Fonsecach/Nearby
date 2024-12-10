628016dc5f942b1f39dc5a49523b498ea8398447
Cleiton <chdf@proton.me>
2024-12-09 22:56:37 -0300

    Create Project React Native App

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{fontSize: 24}}>Hello World</Text>
+            <Text>Meu Primeiro projeto em React Native</Text>
+        </View>
+    )
+}
